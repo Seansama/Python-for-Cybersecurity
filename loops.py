522effc2d7cf473152d42b7fe59ee15cf8a77f47
Shaun Mwangi <Shaun.mwangi@student.moringaschool.com>
2023-08-09 10:24:18 +0300

    loops

---
+++ loops.py
@@ -0,0 +1,10 @@
+string = 'Hello World!'
+for i in string:
+    print(i)
+
+cars = ['Ferrari', 'Mercedes', 'Aston Martin', 'Bugatti', 'Toyota', 'Honda']
+for j in cars:
+    print(j)
+
+arr = [['one', 'two', 'three'], ['Lewis', 'Max', 'Lando']]
+print(arr[0][2], arr[1][0])
