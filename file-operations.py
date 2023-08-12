846255af000149125ba62a9adbd0f5b161562f9d
Shaun Mwangi <Shaun.mwangi@student.moringaschool.com>
2023-08-12 22:01:37 +0300

    Append a file and OS module

--- file-operations.py
+++ file-operations.py
@@ -1,11 +1,11 @@
 # Read from a file
 
-file = open('assets/myfile.txt', 'r')
+file = open('assets/file.txt', 'r')
 data = file.read()
 file.close()
 print(data)
 # Reading from a file using a with statement
-with open('assets/myfile.txt', 'r') as f:
+with open('assets/file.txt', 'r') as f:
     try:
         content = f.read()
     except FileNotFoundError:
@@ -15,10 +15,20 @@ with open('assets/myfile.txt', 'r') as f:
 
 # Writing into files
 
-file_a = open('assets/myfile.txt', 'w')
+file_a = open('assets/file.txt', 'w')
 file_a.write(input('Please type some text: '))
 file_a.close()
 
 # Writing into file using with statement
-with open('assets/myfile.txt', 'w') as f:
+with open('assets/file.txt', 'w') as f:
     f.write(input('Please type some text: '))
+
+# Append into files
+file_b = open('assets/file.txt', 'a')
+file_b.write(input('Add some text: '))
+file_b.close()
+
+# Append into files using with statement
+with open('assets/file.txt', 'a') as f:
+    f.write(input('Add some text: '))
+
