f4d274f887840a191862c13270873b03a2d5c474
Shaun Mwangi <Shaun.mwangi@student.moringaschool.com>
2023-08-13 20:27:19 +0300

    Object oriented programming

---
+++ Intermediate/object_oriented_programming.py
@@ -0,0 +1,29 @@
+class Person:
+
+    amount = 0  # Class variables. Applies to ALL instances of a class
+
+    def __init__(self, name, age, height):  # Constructor function
+        self.name = name
+        self.age = age
+        self.height = height
+        Person.amount += 1
+
+    def __del__(self):  # Destructor function
+        Person.amount -= 1
+        print('Object deleted')
+
+    def __str__(self):  # Print object as string
+        return 'Name: {}, Age: {}, Height: {}'.format(self.name, self.age, self.height)
+
+
+person1 = Person('Shaun', 19, 175)  # Instance of class Person
+print(person1.name, person1.age, person1.height)
+
+person1.name = 'Allen'  # Reassigning an instance attribute
+print(person1.name)
+
+# del person1
+print(person1)
+person2 = Person('Shaun', 21, 185)
+print(Person.amount)
+
