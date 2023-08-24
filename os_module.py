d42de2d1580470e9fcdac590112f077cd1c58184
Shaun Mwangi <Shaun.mwangi@student.moringaschool.com>
2023-08-24 17:56:53 +0300

    Magic methods

--- os_module.py
+++
@@ -1,7 +0,0 @@
-from os import *
-
-# mkdir('')  # Make a new directory
-# chdir('')  # Change directory
-# rename('assets/myfile.txt', 'assets/file.txt')  # Rename a file
-rmdir('test')  # Remove a directory
-# remove('') # Remove a files
